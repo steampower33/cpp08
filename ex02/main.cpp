21d4ce69d3c36cf492e7e99f3406a53680c0bddb
SeungMinLee <seunlee2@student.42seoul.kr>
2024-01-27 18:09:35 +0900

    tester

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "MutantStack.hpp"
+#include <vector>
 
 int main()
 {
@@ -15,6 +16,21 @@ int main()
 	if (!mstack.empty())
 		std::cout << "mstack is not empty" << std::endl;
 
+	std::vector<int> v;
+	v.push_back(5);
+	v.push_back(10);
+	v.push_back(15);
+	v.push_back(20);
+	v.push_back(25);
+	std::cout << "top: " << v.at(v.size() - 1) << std::endl;
+	std::cout << "size: " << v.size() << std::endl;
+	v.pop_back();
+	std::cout << "size: " << v.size() << std::endl;
+	if (!v.empty())
+		std::cout << "vector is not empty" << std::endl;
+	
+	std::cout << "-----------------------------------------------" << std::endl;
+	
 	std::cout << "<< iterator >>" << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -25,6 +41,16 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
+	std::cout << "<< vector iterator >>" << std::endl;
+	std::vector<int>::iterator vit = v.begin();
+	std::vector<int>::iterator vite = v.end();
+	while (vit != vite)
+	{
+		std::cout << *vit << std::endl;
+		++vit;
+	}
+
+	std::cout << "-----------------------------------------------" << std::endl;
 
 	std::cout << "<< reverse_iterator >>" << std::endl;
 	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
@@ -33,6 +59,15 @@ int main()
 	{
 		std::cout << *rit << std::endl;
 		++rit;
+	}
+
+	std::cout << "<< vector reverse_iterator >>" << std::endl;
+	std::vector<int>::reverse_iterator vrit = v.rbegin();
+	std::vector<int>::reverse_iterator vrite = v.rend();
+	while (vrit != vrite)
+	{
+		std::cout << *vrit << std::endl;
+		++vrit;
 	}
 
 	return 0;
